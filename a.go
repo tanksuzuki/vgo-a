6db14ea5ee3e2f8b810d94c3ce4a4ab16eec1da5
Asuka Suzuki <hello@tanksuzuki.com>
2018-04-06 00:36:29 +0900

    1.0.0

---
+++ a.go
@@ -0,0 +1,12 @@
+package a // import "github.com/tanksuzuki/vgo-a"
+
+import (
+	"fmt"
+
+	"github.com/tanksuzuki/vgo-c"
+)
+
+func Hello() {
+	fmt.Println("1.0.0")
+	c.Hello()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module "github.com/tanksuzuki/vgo-a"
+
+require "github.com/tanksuzuki/vgo-c" v1.0.0
